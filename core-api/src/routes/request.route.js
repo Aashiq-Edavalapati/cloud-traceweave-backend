a2f0b066c92f17a3d1cf788ca7705f06d7bdbda9
Aashiq Edavalapati <aashiqedavalapati58@gmail.com>
2026-02-21 22:46:55 +0530

    feat: make form-data in http body functional along with file upload support

--- core-api/src/routes/request.route.js
+++ core-api/src/routes/request.route.js
@@ -3,12 +3,15 @@ import { requestController } from '../controllers/request.controller.js';
 import authenticateUser from '../middlewares/auth.middleware.js';
 import { requireWorkspaceRole } from '../middlewares/rbac.middleware.js';
 import { cookieController } from '../controllers/cookie.controller.js';
+import multer from 'multer';
 
 const router = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.use(authenticateUser);
 
-router.post('/execute', requireWorkspaceRole('EDITOR'), requestController.executeAdHocRequest);
+router.post('/execute', requireWorkspaceRole('EDITOR'), upload.any(), requestController.executeAdHocRequest);
 
 router.get('/jar/cookies', cookieController.getCookies);
 
@@ -24,7 +27,7 @@ router.patch('/:requestId', requireWorkspaceRole('EDITOR'), requestController.up
 
 router.delete('/:requestId', requireWorkspaceRole('OWNER'), requestController.deleteRequest);
 
-router.post('/:requestId/send', requireWorkspaceRole('EDITOR'), requestController.sendRequest);
+router.post('/:requestId/send', requireWorkspaceRole('EDITOR'), upload.any(), requestController.sendRequest);
 
 router.get('/:requestId/history', requireWorkspaceRole('VIEWER'), requestController.getRequestHistory);
 
